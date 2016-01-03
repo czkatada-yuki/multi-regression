4696e4a5568fd60969361f43f7e913071a29340d
Yuki Katada <ponyo.ky@gmail.com>
2016-01-03 15:43:08 +0900

    delete regress in linear regression function

--- src/main/scala/logics/LinearRegression.scala
+++ src/main/scala/logics/LinearRegression.scala
@@ -1,14 +1,12 @@
 package logics
 
 import breeze.linalg.{ DenseMatrix, DenseVector }
+import utils.CSVReader
 
 /**
  * Created by katadayuuki on 1/2/16.
  */
 object LinearRegression {
-  def regress(observations: DenseMatrix[Double], outputs: DenseVector[Double]): DenseVector[Double] = {
-    val cov = DenseMatrix.zeros[Double](observations.cols, observations.cols) + (observations.t * observations)
-    val scaled = DenseVector.zeros[Double](observations.cols) + (observations.t * outputs)
-    cov \ scaled
+  def run = {
   }
 }
